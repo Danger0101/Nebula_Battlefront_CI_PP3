925ebd8948cc0919dec7c749001985d83aa9b0d1
John Hummel <34178990+Danger0101@users.noreply.github.com>
2023-10-06 16:33:04 +0100

    Add basic code structure
    Added added all the class's and functions with the doc strings that will be needed for the project as I see it.

--- run.py
+++ run.py
@@ -1,3 +1,127 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""
+This module defines the SpaceshipGame class
+and the main function to run the game.
+"""
+
+pass
+
+def clear_terminal():
+    """
+    Clear the terminal screen.
+    """
+    pass
+
+def typewriter_effect(text, delay=0.05):
+    """
+    Display text with a typewriter effect.
+
+    Args:
+        text (str): The text to be displayed.
+        delay (float, optional): The delay between printing each character.
+
+    Returns:
+        None
+    """
+    pass
+
+class TitleScreen:
+    """
+    Represents the title screen of the game.
+    """
+    pass
+
+    def display(self):
+        """
+        Display the title screen and ask if the user wants to play the game.
+        """
+        pass
+
+class Board:
+    """
+    Represents the game board.
+    """
+    pass
+
+class Ship:
+    """
+    Represents a spaceship on the board.
+    """
+    def __init__(self, owner, coordinates):
+        """
+        Initialize a spaceship.
+
+        Args:
+            owner (str): The owner of the spaceship.
+            coordinates (list of tuple): The coordinates occupied by the spaceship.
+        """
+        pass
+
+class User:
+    """
+    Represents a user in the game.
+    """
+    def __init__(self, name, board_size, num_ships):
+        """
+        Initialize a user.
+
+        Args:
+            name (str): The name of the user.
+            board_size (int): The size of the game board.
+            num_ships (int): The number of spaceships the user wants to place.
+        """
+        pass
+
+
+    def place_ships(self):
+        """
+        Determine how the user wants to place their spaceships and place them accordingly.
+        """
+        pass
+
+    def place_ships_manually(self):
+        """
+        Manually place the user's spaceships on the board.
+        """
+        pass
+
+    def place_ships_randomly(self):
+        """
+        Place the user's spaceships randomly on the board.
+        """
+        pass
+
+    def get_valid_input(self):
+        """
+        Get valid input for coordinates from the user.
+
+        Returns:
+            tuple: A tuple containing the x_coordinate and y_coordinate coordinates.
+        """
+        pass
+
+class SpaceshipGame:
+    """
+    Represents a game of Spaceship.
+    """
+    pass
+
+def display_win_art():
+    """
+    Display a winning message in ASCII art.
+    """
+    pass
+
+def display_loss_art():
+    """
+    Display a losing message in ASCII art.
+    """
+    pass
+
+def main():
+    """
+    Define the main function to run the game.
+    """
+    pass
+
+if __name__ == "__main__":
+    main()
